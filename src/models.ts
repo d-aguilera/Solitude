3029a8eeb5d2e49d8a0270bf32d92d7eab3e537a
Daniel Aguilera <daniel.aguilera@blackstone.com>
2026-01-02 17:25:46 -0500

    Comment airplane model

--- src/models.ts
+++ src/models.ts
@@ -5,29 +5,29 @@ export const AIRPLANE = "AIRPLANE";
 export const airplaneModel: Model = {
   objectType: AIRPLANE,
   points: [
-    { x: 0, y: 0.5, z: 0 },
-    { x: 0, y: 0.1, z: 0.15 },
-    { x: -0.1, y: 0, z: 0 },
-    { x: 0.1, y: 0, z: 0 },
-    { x: 0, y: 0, z: -0.1 },
-    { x: 0, y: -0.5, z: 0 },
-    { x: -0.5, y: -0.3, z: 0 },
-    { x: 0.5, y: -0.3, z: 0 },
-    { x: 0, y: -0.5, z: 0.3 },
+    { x: 0, y: 0.5, z: 0 }, // 0: nose tip
+    { x: 0, y: 0.1, z: 0.15 }, // 1: fuselage top center
+    { x: -0.1, y: 0, z: 0 }, // 2: fuselage left center
+    { x: 0.1, y: 0, z: 0 }, // 3: fuselage right center
+    { x: 0, y: 0, z: -0.1 }, // 4: fuselage bottom center
+    { x: 0, y: -0.5, z: 0 }, // 5: tail tip
+    { x: -0.5, y: -0.3, z: 0 }, // 6: left wing tip
+    { x: 0.5, y: -0.3, z: 0 }, // 7: right wing tip
+    { x: 0, y: -0.5, z: 0.3 }, // 8: vertical stabilizer tip
   ],
   lines: [
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-    [1, 5],
-    [2, 5],
-    [3, 5],
-    [4, 5],
-    [1, 2, 4, 3],
-    [2, 6, 5],
-    [3, 7, 5],
-    [1, 8, 5],
+    [0, 1], // nose to fuselage top center
+    [0, 2], // nose to fuselage left center
+    [0, 3], // nose to fuselage right center
+    [0, 4], // nose to fuselage bottom center
+    [1, 5], // fuselage top center to tail tip
+    [2, 5], // fuselage left center to tail tip
+    [3, 5], // fuselage right center to tail tip
+    [4, 5], // fuselage bottom center to tail tip
+    [1, 2, 4, 3], // fuselage middle ring
+    [2, 6, 5], // left wing
+    [3, 7, 5], // right wing
+    [1, 8, 5], // vertical stabilizer
   ],
   color: { r: 0, g: 255, b: 255 },
   lineWidth: 1,
